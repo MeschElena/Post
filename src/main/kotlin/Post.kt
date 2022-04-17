8f50b0b98aff7c52a3539c61a3361185829b7831
MeschElena <narelena@yandex.ru>
2022-04-17 16:53:26 +0400

    Revert "add zadanie 2.3"
    This reverts commit 1c2c8522d3e97f5b8b58c0a10d3068544a6928be.

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -4,7 +4,9 @@ data class Comments(
     val groupsCanPost: Int,
     val canClose: Boolean,
     val canOpen:Boolean
-)
+    ) {
+
+}
 
 data class Copyright(
     val id: Int,
@@ -18,7 +20,9 @@ data class Likes(
     val userLikes: Boolean,
     val canLike: Boolean,
     val canPublish: Boolean
-)
+) {
+
+}
 
 data class Reposts(
     val count: Int,
@@ -29,35 +33,7 @@ data class Views(
     val count: Int
 )
 
-data class PostSource(
-    val type: String,
-    val platform: String,
-    val data: String,
-    val url: String
-)
-
-data class Place(
-    val id: Int,
-    val title: String,
-    val latitude: Int,
-    val longitude: Int,
-    val created: Int,
-    val icon: String,
-    val checkins: Int,
-    val updated: Int,
-    val type: Int,
-    val country: Int,
-    val city: Int,
-    val address: String
-)
-
-data class Geo(
-    val type: String,
-    val coordinates: String,
-    val place: Place?,
-)
-
-data class Post(
+data class Post (
     val id: Int = 0,
     val ownerId: Int,
     val fromId: Int,
@@ -72,11 +48,7 @@ data class Post(
     val reposts: Reposts,
     val views: Views,
     val postType: String,
-    val postSource: PostSource?,
-    val attachment: Array<Attachment>?,
-    val geo: Geo,
     val signerId: Int,
-    val copyHistory: Array<Post>?,
     val canPin: Boolean,
     val canDelete: Boolean,
     val canEdit: Boolean,
@@ -84,4 +56,7 @@ data class Post(
     val markedAsAds: Boolean,
     val isFavorite: Boolean,
     val postponedId: Int
-)
+
+){
+
+}
